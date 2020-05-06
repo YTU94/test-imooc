dc7255e03dabd02218f284169d7096ab48f8345c
linyi <linyi@tsign.cn>
2020-05-06 17:38:51 +0800

    change

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app">
         <img alt="Vue logo" src="./assets/logo.png" />
-        <HelloWorld msg="Welcome to Your Vue.js App 0 1"  />
+        <HelloWorld msg="Welcome to Your Vue.js App 0 1 2"  />
     </div>
 </template>
 
